ea186d8728e2e512a0a29e7dadc996fc006b08cd
Marcos Snaider <00020923@fepi.edu.br>
2023-11-09 17:33:31 -0300

    Api_Git_Hub

---
+++ public/main.js
@@ -0,0 +1,26 @@
+const gitHubId = document.querySelector('input[type=text]')
+const btnBuscar = document.querySelector('input[type=button')
+btnBuscar.onclick = function(){
+    //Fazendo a conexao com a API
+    fetch('https://api.github.com/users/'+ gitHubId.value)
+    .then(response => {
+        if(response.ok){
+            return response.json()
+        }
+        else{
+            throw new Error('Erro na requisição')
+        }
+    })
+    .then(data => {
+        document.querySelector('#name').textContent = data.name
+        document.querySelector('#bio').textContent = data.bio
+        document.querySelector('#followers').textContent = data.followers
+        document.querySelector('#location').textContent = data.location
+        document.querySelector('#avatar').src = data.avatar_url
+        //E outros atributos que voce desejar
+
+    })
+    .catch(error =>{
+        console.log('Erro:' + error)
+    })
+}
